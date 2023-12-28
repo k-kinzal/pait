6b78885459f2e355dc48c4c4ae8290631b6fb7ce
k-kinzal <log@logn.in>
2023-12-28 10:07:45 +0900

    init style

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: root element is always present
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<CssBaseline>
+			<App />
+		</CssBaseline>
 	</React.StrictMode>,
 );
