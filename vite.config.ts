c708e020a3a680bc414a769e30968a95404df2be
k-kinzal <log@logn.in>
2023-12-28 09:59:13 +0900

    init vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [react()],
 })
