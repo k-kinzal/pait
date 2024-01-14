4021ac7a2350adb29b637c7e0ab02202607ff88c
k-kinzal <log@logn.in>
2024-01-14 15:12:49 +0900

    create main page component

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,44 @@
-import { ForwardedRef, forwardRef, ReactElement } from "react";
-import { Box, Container, IconButton } from "@mui/material";
+import {
+  ForwardedRef,
+  forwardRef,
+  PropsWithChildren,
+  ReactElement,
+} from "react";
+import { Box, Container, IconButton, IconButtonProps } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { CalculatePane } from "./CalculatePane/CalculatePane";
 import { PaymentPane } from "./PaymentPane/PaymentPane";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
+import { BoxProps } from "@mui/material/Box/Box";
 
 export interface Props {}
 
 export const MainPage = forwardRef(
   (_: Props, ref: ForwardedRef<HTMLDivElement>): ReactElement => {
     return (
-      <Box width={1} height={1} ref={ref}>
+      <Layout ref={ref}>
+        <Content header={<LockIconButton />}>
+          <CalculatePane />
+        </Content>
+        <Content>
+          <PaymentPane />
+        </Content>
+      </Layout>
+    );
+  },
+);
+
+interface LayoutProps extends Omit<BoxProps, "children"> {
+  children: ReactElement[];
+}
+
+export const Layout = forwardRef(
+  (
+    { children, ...props }: LayoutProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ): ReactElement => {
+    return (
+      <Box width={1} height={1} ref={ref} {...props}>
         <Container maxWidth={"md"} sx={{ height: 1 }}>
           <Grid
             container
@@ -22,58 +50,93 @@ export const MainPage = forwardRef(
               placeItems: "center",
             }}
           >
-            <Grid xs={1} height="80%">
-              <Box
-                minWidth="365px"
-                maxWidth="380px"
-                width={1}
-                height={1}
-                p={5}
-                boxSizing="border-box"
+            {children.map((child, index) => (
+              <Grid
+                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                key={index}
+                xs={1}
+                height="80%"
                 sx={{
-                  borderRadius: "16px",
-                  backgroundColor: "black",
-                  overflow: "hidden",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                <Box position="relative">
-                  <Box position="absolute" mt={-4} ml={-4}>
-                    <IconButton
-                      sx={{
-                        width: "26px",
-                        height: "26px",
-                        minHeight: "26px",
-                        backgroundColor: "black",
-                        color: "grey.500",
-                      }}
-                    >
-                      <LockOpenIcon />
-                    </IconButton>
-                  </Box>
-                  <CalculatePane />
-                </Box>
-              </Box>
-            </Grid>
-            <Grid xs={1} height="80%">
-              <Box
-                minWidth="365px"
-                maxWidth="380px"
-                width={1}
-                height={1}
-                p={5}
-                boxSizing="border-box"
-                sx={{
-                  borderRadius: "16px",
-                  backgroundColor: "black",
-                  overflow: "hidden",
-                }}
-              >
-                <PaymentPane />
-              </Box>
-            </Grid>
+                {child}
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </Box>
+    );
+  },
+);
+
+interface ContentProps extends BoxProps {
+  header?: ReactElement;
+}
+
+const Content = forwardRef(
+  (
+    { children, header, ...props }: ContentProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ): ReactElement => {
+    return (
+      <Box
+        minWidth="365px"
+        maxWidth="380px"
+        width={1}
+        height={1}
+        p={5}
+        boxSizing="border-box"
+        sx={{
+          borderRadius: "16px",
+          backgroundColor: "black",
+          overflow: "hidden",
+        }}
+        ref={ref}
+        {...props}
+      >
+        {header && (
+          <Box position="absolute" mt={-2} ml={-2}>
+            {header}
+          </Box>
+        )}
+        <Box
+          width={1}
+          height={1}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {children}
+        </Box>
+      </Box>
+    );
+  },
+);
+
+const LockIconButton = forwardRef(
+  (
+    { children, ...props }: PropsWithChildren<IconButtonProps>,
+    ref: ForwardedRef<HTMLButtonElement>,
+  ): ReactElement => {
+    return (
+      <IconButton
+        sx={{
+          width: "26px",
+          height: "26px",
+          minHeight: "26px",
+          backgroundColor: "black",
+          color: "grey.500",
+        }}
+        ref={ref}
+        {...props}
+      >
+        <LockOpenIcon />
+      </IconButton>
     );
   },
 );
